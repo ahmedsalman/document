f21685625d0abf508ab95c03ac1075d5c855ee43
Ahmed Salman <ahmed.salman.noor@gmail.com>
2015-08-03 01:57:31 +0400

    hibernate base model in other models

--- models.py
+++ models.py
@@ -8,6 +8,26 @@ from django.utils.translation import ugettext as _
 from taggit.managers import TaggableManager
 
 
+class TimeStampAwareModel( models.Model ):
+    """
+    A model class that can be used as super class
+    for any model that is considered timestamp aware 
+    model.
+    """
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    date_deleted = models.DateTimeField(blank=True, null=True)
+
+    def is_deleted(self):
+        if self.date_deleted:
+            return True
+        return False
+    is_deleted.boolean = True
+    
+    class Meta:
+        abstract = True
+
+
 class Document( TimeStampAwareModel ):
     """
     Document model
